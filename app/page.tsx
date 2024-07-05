b0bcb2208223fd8093193d86f54485cfa4f0bcc9
Doug Szeto <douglas.szeto@slalom.com>
2024-07-05 15:14:09 -0400

    remove tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Loader from "@/components/Loader";
+import Navbar from "@/components/NavBar";
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Loader show={true} />
       <Link prefetch={false} href="/dougsgrubs">
         Doug&apos;s profile
---
+++ components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function Navbar({}) {
+  const { user, username } = {};
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button>FEED</button>
+          </Link>
+        </li>
+        {/* user is signed in and has username */}
+        {username && (
+          <>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+        {/* user is not signed in OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button>Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
