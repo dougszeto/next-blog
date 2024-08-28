48712e2709929c047f109b51e4632776b76a64bd
Doug Szeto <dszeto615@gmail.com>
2024-08-28 16:42:50 -0400

    updates metadata for homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 import Providers from "@/components/Providers";
+import { createMetadata } from "@/lib/metadata";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Doug's Grubs",
-  description: "A food blog by Doug",
-  twitter: {
-    images: { url: 'https://i.pinimg.com/736x/5e/99/52/5e99520776c93392211f7d41f043ed32.jpg', alt: 'hamburger emoji' }
-  }
-};
+export async function generateMetadata() {
+  return createMetadata({
+    title: "Doug's Grubs",
+    description: "A food blog by Doug",
+    image: 'https://i.pinimg.com/736x/5e/99/52/5e99520776c93392211f7d41f043ed32.jpg', 
+    imageAlt: 'hamburger emoji' 
+  });
+}
 
 export default function RootLayout({
   children,
